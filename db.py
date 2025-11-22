f7d8f6af09e51a02dedff0d1dc23cef733d6cd6f
Geronimo1106 <161893398+Geronimo1106@users.noreply.github.com>
2025-11-22 20:32:31 +0100

    addition of database

--- db.py
+++ db.py
@@ -1,8 +1,16 @@
 import sqlite3
 #https://docs.python.org/3/library/sqlite3.html
+#https://www.w3schools.com/sql/sql_autoincrement.asp
 
 DB_FILE = "passwords.db"
 
+def connect():
 con = sqlite3.connect(DB_FILE)
 cur = con.cursor()
-cur.execute("CREATE TABLE movie(title, year, score)")
+cur.execute("""CREATE TABLE IF NOT EXISTS password_entries(
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            service TEXT NOT NULL,
+            username TEXT NOT NULL,
+            password TEXT NOT NULL,
+            created_at TEXT NOT NULL,
+            updated_at TEXT NOT NULL)""")
